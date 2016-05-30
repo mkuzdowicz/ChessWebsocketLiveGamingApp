805ce01d9c7ef3e79316d7d5d4ef7b4cd1c3be02
MartinKuzdowicz <martinkuzdowicz@gamil.com>
2016-05-31 00:06:08 +0200

    first

---
+++ src/main/java/com/kuzdowicz/livegaming/ChessLiveGamingAppApplication.java
@@ -0,0 +1,12 @@
+package com.kuzdowicz.livegaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessLiveGamingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessLiveGamingAppApplication.class, args);
+	}
+}
---
+++ src/main/java/com/kuzdowicz/livegaming/config/SpringSecurityConf.java
@@ -0,0 +1,35 @@
+package com.kuzdowicz.livegaming.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConf extends WebSecurityConfigurerAdapter {
+	
+	
+	@Override
+	public void configure(WebSecurity webSecurity) throws Exception {
+
+		webSecurity.ignoring().antMatchers("/resources/**");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http//
+				.formLogin()//
+				.loginPage("/login")//
+				.defaultSuccessUrl("/user/tutorials-to-do")//
+				.and()//
+				.authorizeRequests()//
+				.antMatchers("/user/**")//
+				.hasAnyRole("USER", "ADMIN")//
+				.antMatchers("/admin/**").hasRole("ADMIN")//
+				.anyRequest().permitAll();
+	}
+
+}
